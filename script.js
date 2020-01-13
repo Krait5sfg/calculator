689e217d573ede314806e05f3a6b7367456c437a
Yura <yurii_kulachenko@mail.ru>
2020-01-14 00:34:06 +0300

    work progress 10%

--- script.js
+++ script.js
@@ -20,6 +20,18 @@ function operate(operator, number1, number2) {
     return result;
 }
 
-//test
-let test = operate(divide, 1, 2);
-console.log(test);
+//get digit
+const digitNode = document.querySelectorAll('.digit');
+//array for saving digit numbers
+let numberArray = [];
+//get a display
+const disp = document.querySelector('.display');
+//make a element with number to fill the display
+digitNode.forEach(element => {
+    element.addEventListener('click', (event) => {
+        disp.textContent += element.getAttribute('data-info');
+        numberArray.push(element.getAttribute('data-info'));
+        console.log(numberArray);
+    });
+});
+
