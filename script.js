0b35ca1b84fe2f1c669c8483007a5589b2c9b2fc
Yura <yurii_kulachenko@mail.ru>
2020-01-14 23:07:37 +0300

    calculate 2 number

--- script.js
+++ script.js
@@ -20,18 +20,68 @@ function operate(operator, number1, number2) {
     return result;
 }
 
-//get digit
+//get digit number
 const digitNode = document.querySelectorAll('.digit');
-//array for saving digit numbers
+//get operator number
+const operNode = document.querySelectorAll('.operate');
+//array for saving numbers pressed on calculator
 let numberArray = [];
+//array for saving operation name
+let operationArray = [];
+//variable for reuslt
+let result = 0;
 //get a display
 const disp = document.querySelector('.display');
-//make a element with number to fill the display
+//display number
 digitNode.forEach(element => {
     element.addEventListener('click', (event) => {
         disp.textContent += element.getAttribute('data-info');
-        numberArray.push(element.getAttribute('data-info'));
-        console.log(numberArray);
     });
 });
+//function on operator
+operNode.forEach(element => {
+    element.addEventListener('click', (event) => {
+        switch (element.getAttribute('data-info')) {
+            case 'clear':
+                numberArray = [];
+                operationArray = [];
+                disp.textContent = '';
+                break;
+            case 'add':
+            case 'divide':
+            case 'subtract':
+            case 'multiply':
+                numberArray.push(Math.abs(disp.textContent)); //save number not string
+                operationArray.push(element.getAttribute('data-info'));
+                disp.textContent = '';
+                break;
+            case 'equal':
+                numberArray.push(Math.abs(disp.textContent));//save number not string
+                //calculate (2 numbers)
+                if (numberArray.length == 2 && operationArray.length == 1) {
+                    operationArray.forEach(element => {
+                        if (element == 'add') {
+                            disp.textContent = operate(add, ...numberArray);
+                        } else if (element == 'multiply') {
+                            disp.textContent = operate(multiply, ...numberArray);
+                        } else if (element == 'subtract') {
+                            disp.textContent = operate(subtract, ...numberArray);
+                        } else if (element == 'divide') {
+                            disp.textContent = operate(divide, ...numberArray);
+                        }
+                    });
+                }
+                //calculate greater than 2 numbers
+                if (numberArray.length > 2 && operationArray.length > 1) {
+                    //..code here
+                }
+                //reset on zero after the end calculation
+                numberArray = [];
+                operationArray = [];
+                console.log(result);
 
+        }
+
+        console.log('numberArray: ', numberArray, 'operationArray: ', operationArray);
+    });
+});
